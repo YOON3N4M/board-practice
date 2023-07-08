3d0fb49c110928f8f1514fac04533404b9c3d56f
yoon3nam <lgmysn2@gmail.com>
2023-07-08 17:14:47 +0900

    feat: 네비게이션 추가

---
+++ src/pages/register.tsx
@@ -0,0 +1,12 @@
+export default function Register() {
+  return (
+    <>
+      <div>
+        <form>
+          <label>아이디</label>
+          <input></input>
+        </form>
+      </div>
+    </>
+  );
+}
