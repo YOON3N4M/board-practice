5f02a160930cea0a02e4d6250ca7b145284573c3
yoon3n4m <lgmysn2@gmail.com>
2023-07-12 16:48:33 +0900

    fix: npx prisma generate 이후 재배포

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  async rewrites() {
-    return [
-      {
-        source: "/:path*",
-        destination: "https://board-practice.vercel.app/:path*",
-      },
-    ];
-  }
+ 
 };
 
 module.exports = nextConfig;
