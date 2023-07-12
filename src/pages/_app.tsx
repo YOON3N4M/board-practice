fabe0b504128546b05153d0042e002dfd2f0b18b
yoon3n4m <lgmysn2@gmail.com>
2023-07-12 15:57:26 +0900

    feat: cors 문제때문에 next의 rewrite로 해결 했음

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
 
-export const API_URL_USER_DATA = "http://localhost:3000/api/users/user-data";
+export const API_URL_USER_DATA = "/api/users/user-data";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
