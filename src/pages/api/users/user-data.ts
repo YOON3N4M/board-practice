8bf2b715be18a4cb515b6cac09af8798591005fb
yoon3n4m <lgmysn2@gmail.com>
2023-07-14 13:20:17 +0900

    fix: deleteMany로 모든 필드를 삭제하던 문제 수정

--- src/pages/api/users/user-data.ts
+++ src/pages/api/users/user-data.ts
@@ -6,6 +6,7 @@ export default async function handler(
   res: NextApiResponse
 ) {
   const { id, name, account, password, sex } = req.body;
+
   if (req.method === "POST") {
     try {
       const user = await prisma.user.create({
@@ -30,9 +31,9 @@ export default async function handler(
     }
   } else if (req.method === "DELETE") {
     try {
-      const deleteTarget = await prisma.user.deleteMany({
+      const deleteTarget = await prisma.user.delete({
         where: {
-          id: id,
+          id: req.body,
         },
       });
       res.status(200).json({ message: "200, 삭제 성공" });
