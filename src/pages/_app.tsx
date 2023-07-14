8f895d0988934178c36bc2d7fb6d7a85d89d5907
yoon3n4m <lgmysn2@gmail.com>
2023-07-14 13:38:03 +0900

    docs: userData api의 엔드포인트 변경

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import Layout from "@/components/Layout";
 import type { AppProps } from "next/app";
 
-export const API_URL_USER_DATA = "/api/users/user-data";
+export const API_URL_USER_DATA = "/api/user-data";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
