a053aef53faf3cbad82db7d8d47c6b6ff6bf58e8
yoon3n4m <lgmysn2@gmail.com>
2023-07-17 16:40:47 +0900

    design: 네비게이터 여백 수정 (임시)

--- src/components/Navigator.tsx
+++ src/components/Navigator.tsx
@@ -2,29 +2,40 @@ import Link from "next/link";
 import { styled } from "styled-components";
 
 const NavigatorContainer = styled.div`
+  width: 100vw;
+  //background-color: red;
+  padding: 1rem 0;
+`;
+
+const Navi = styled.div`
   display: flex;
   margin: 0 auto;
   width: 400px;
   justify-content: space-between;
-  background-color: blue;
+  // background-color: blue;
 `;
 
 export default function Navigator() {
   return (
     <>
       <NavigatorContainer>
-        <Link legacyBehavior href="/">
-          <a>홈</a>
-        </Link>
-        <Link legacyBehavior href="/login">
-          <a>로그인</a>
-        </Link>
-        <Link legacyBehavior href="/register">
-          <a>회원가입</a>
-        </Link>
-        <Link legacyBehavior href="/user-list">
-          <a>회원목록</a>
-        </Link>
+        <Navi>
+          <Link legacyBehavior href="/">
+            <a>홈</a>
+          </Link>
+          <Link legacyBehavior href="/map">
+            <a>맵</a>
+          </Link>
+          <Link legacyBehavior href="/login">
+            <a>로그인</a>
+          </Link>
+          <Link legacyBehavior href="/register">
+            <a>회원가입</a>
+          </Link>
+          <Link legacyBehavior href="/user-list">
+            <a>회원목록</a>
+          </Link>
+        </Navi>
       </NavigatorContainer>
     </>
   );
