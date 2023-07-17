f14309267f986edd8e919da4c22e711c0cc87fc8
yoon3n4m <lgmysn2@gmail.com>
2023-07-17 16:30:47 +0900

    fix: vercel 배포에서 api key가 undefined로 뜨는 문제 해결 #1

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
   reactStrictMode: true,
- 
+  env: {
+    KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+  },
 };
 
 module.exports = nextConfig;
