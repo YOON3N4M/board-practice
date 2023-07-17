39c875f5292f6b109034ae1fadfd20263d1e5848
yoon3n4m <lgmysn2@gmail.com>
2023-07-17 10:13:46 +0900

    feat: 반응형 Auth form 구현, register페이지에서 선언후 export
    login에서도 해당 폼을 재사용하여 적용
    styled-components의 props명에는 대문자를 권장하지 않기에 camelCase가
    아닌 그냥 전부 소문자로 변경

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,41 +1,16 @@
 import { styled } from "styled-components";
 
-export default function Home() {
-  return (
-    <>
-      <StyledSection backgroundColor="#e5bc7187" flexDirection="column">
-        <h1>지도로 한눈에 보는 우리 모임의 추억</h1>
-        <span>저번에 갔던 숙소 어디였지?, 이 음식 어디서 먹었더라?</span>
-        <span>여행을 함께 재밌게 기록할 수 있는 방법 없을까?</span>
-        <div>
-          <button>로그인</button>
-          <button>회원가입</button>
-        </div>
-      </StyledSection>
-
-      <StyledSection flexDirection="row">
-        <MainPageImage></MainPageImage>
-        <h2>추억의 장소를 함께 저장해요</h2>
-      </StyledSection>
-
-      <StyledSection flexDirection="row">
-        <h2>장소에 대한 이야기를 나눠요</h2>
-      </StyledSection>
-    </>
-  );
-}
-
 const StyledSection = styled.section<{
-  backgroundColor?: string;
-  flexDirection: string;
+  backgroundcolor?: string;
+  flexdirection: string;
 }>`
   display: flex;
-  flex-direction: ${props => props.flexDirection};
+  flex-direction: ${props => props.flexdirection};
   width: 100vw;
   border: 1px solid black;
   padding: 15rem 0;
   background-color: ${props =>
-    props.backgroundColor ? props.backgroundColor : "white"};
+    props.backgroundcolor ? props.backgroundcolor : "white"};
 
   * {
     text-align: center;
@@ -47,3 +22,28 @@ const MainPageImage = styled.div`
   width: 20%;
   background-color: blue;
 `;
+
+export default function Home() {
+  return (
+    <>
+      <StyledSection backgroundcolor="#e5bc7187" flexdirection="column">
+        <h1>지도로 한눈에 보는 우리 모임의 추억</h1>
+        <span>저번에 갔던 숙소 어디였지?, 이 음식 어디서 먹었더라?</span>
+        <span>여행을 함께 재밌게 기록할 수 있는 방법 없을까?</span>
+        <div>
+          <button>로그인</button>
+          <button>회원가입</button>
+        </div>
+      </StyledSection>
+
+      <StyledSection flexdirection="row">
+        <MainPageImage></MainPageImage>
+        <h2>추억의 장소를 함께 저장해요</h2>
+      </StyledSection>
+
+      <StyledSection flexdirection="row">
+        <h2>장소에 대한 이야기를 나눠요</h2>
+      </StyledSection>
+    </>
+  );
+}
