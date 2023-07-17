4baf6662baa623798eb89cbb2c93769c570a430b
yoon3n4m <lgmysn2@gmail.com>
2023-07-17 16:11:07 +0900

    feat: KakaoMap 컴포넌트화
    KakaoMap으로 호출해서 지도 띄울 수 있음. 현재 옵션이 2가지 밖에 없는데
    추후 기능 공부 후 추가될 수도 있음 (=>type도 함께 수정해야함)

--- src/pages/map/index.tsx
+++ src/pages/map/index.tsx
@@ -1,7 +1,23 @@
+import KakaoMap from "@/components/KaKaoMap";
+import { styled } from "styled-components";
+
+const MapContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+`;
+
 export default function Map() {
+  const defaultMapOption = {
+    center: { lat: 35.98818056, lng: 127.9281444 },
+    level: 12,
+  };
+
   return (
     <>
-      <h1>맵이다.</h1>
+      <MapContainer>
+        <KakaoMap mapOption={defaultMapOption} />
+      </MapContainer>
     </>
   );
 }
