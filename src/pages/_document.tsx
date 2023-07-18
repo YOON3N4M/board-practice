ac43169fb6ec433892d6f01de1882cacfd48d099
yoon3n4m <lgmysn2@gmail.com>
2023-07-18 16:42:45 +0900

    docs: 파일 명 수정

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,9 @@
 import { Html, Head, Main, NextScript } from "next/document";
 import Script from "next/script";
 
+export const KAKAO_API_SERVICE_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_API_KEY}&autoload=false&libraries=services`;
 export default function Document() {
   const KAKAO_API_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_API_KEY}&autoload=false&libraries=services`;
-  const KAKAO_API_SERVICE_URL = `//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.KAKAO_API_KEY}&autoload=false&libraries=services`;
   return (
     <Html lang="en">
       <Head />
