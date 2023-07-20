fb8baf4cbfee75a47d14ba28cd46d99c584a64df
yoon3n4m <lgmysn2@gmail.com>
2023-07-20 16:42:05 +0900

    feat: 카카오 맵 컴포넌트에서 map/index로 주소 값 올리기

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -1,5 +1,5 @@
 import { motion } from "framer-motion";
-import React from "react";
+import React, { useState } from "react";
 import { styled } from "styled-components";
 
 const ModalBackground = styled(motion.div)`
@@ -16,21 +16,45 @@ const ModalBackground = styled(motion.div)`
 const ModalWindow = styled.div`
   position: relative;
   display: flex;
+  justify-content: center;
+  padding: 5rem 0;
   width: 500px;
-  height: 500px;
+
   background-color: white;
   .modal-close {
     position: absolute;
     left: 100%;
+  }
+  .submit-position-form {
+    display: flex;
+    flex-direction: column;
   }
 `;
 
 interface Props {
   isModalOn: boolean;
   setIsModalOn: React.Dispatch<React.SetStateAction<boolean>>;
+  selectedAddress: string;
 }
 
-export default function Modal({ isModalOn, setIsModalOn }: Props) {
+export default function Modal({
+  isModalOn,
+  setIsModalOn,
+  selectedAddress,
+}: Props) {
+  // 테스트 후 이름 바꿔야함
+  function Form() {
+    const [positionName, setPositionName] = useState("");
+    return (
+      <>
+        <form className="submit-position-form">
+          <input placeholder="장소의 별명을 입력하세요"></input>
+          <span>주소:{selectedAddress}</span>
+        </form>
+      </>
+    );
+  }
+
   return (
     <>
       <ModalBackground initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
@@ -38,6 +62,7 @@ export default function Modal({ isModalOn, setIsModalOn }: Props) {
           <button onClick={() => setIsModalOn(false)} className="modal-close">
             닫기
           </button>
+          <Form />
         </ModalWindow>
       </ModalBackground>
     </>
