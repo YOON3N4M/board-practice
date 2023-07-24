493178c5c1edb80ee0f751b850de825785ed1c8f
yoon3n4m <lgmysn2@gmail.com>
2023-07-24 10:35:06 +0900

    refactor: context 타입 지정, 기본값 부여

--- src/util/StateContext.ts
+++ src/util/StateContext.ts
@@ -1,3 +1,23 @@
 import { createContext } from "react";
 
-export const StateContext = createContext<any>(null);
+interface StateContextT {
+  setIsModalOn?: any;
+  selectedAddress?: any;
+  setSelectedAddress?: any;
+  mapDataFromDB?: any;
+  setMapDataFromDB?: any;
+  coords?: any;
+  setCoords?: any;
+}
+
+const DefalutState = {
+  setIsModalOn: undefined,
+  selectedAddress: undefined,
+  setSelectedAddress: undefined,
+  mapDataFromDB: undefined,
+  setMapDataFromDB: undefined,
+  coords: undefined,
+  setCoords: undefined,
+};
+
+export const StateContext = createContext<StateContextT>(DefalutState);
