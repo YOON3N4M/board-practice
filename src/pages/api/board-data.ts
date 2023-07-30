71299e7128820ef14a2a5062270cc8895ec7f869
jpark_0506 <vlrnrhd4@gmail.com>
2023-07-30 20:13:52 +0900

    db : 테이블 컬럼 변경 및 그룹 테이블 인서트 로직 추가

--- src/pages/api/board-data.ts
+++ src/pages/api/board-data.ts
@@ -7,7 +7,6 @@ export default async function handler(
     res: NextApiResponse
   ) {
     const { 
-        main_board_no, 
         main_board_id, 
         user_id, 
         user_account,
@@ -17,8 +16,7 @@ export default async function handler(
         main_board_memo,
         main_board_img,
         group_id,
-        group_theme_id,
-        group_name,
+        group_theme_name,
         location_address,
         main_board_lng,
         main_board_lat,
@@ -37,20 +35,12 @@ export default async function handler(
                 main_board_memo : main_board_memo,
                 main_board_img : main_board_img,
                 group_id : group_id,
-                group_theme_id : group_theme_id,
+                group_theme_name : group_theme_name,
                 location_address : location_address,
                 main_board_lng : main_board_lng,
                 main_board_lat : main_board_lat,
             },
-          });
-          const User_group = await prisma.user_group.create({
-            data: {
-                user_id : user_id, 
-                group_id : group_id,
-                group_theme_id : group_theme_id,
-                group_name : group_name
-            },
-          });
+          })
           res.status(200).json({ message: "200, 등록 성공" });
         } catch (err) {
           res.status(500).json({ message: "500, 등록 실패" });
