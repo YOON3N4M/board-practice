915caa564f63d5cd81eb9c004b2960584bfb1303
yoon3n4m <lgmysn2@gmail.com>
2023-08-17 16:26:34 +0900

    db: prisma adapter 관련 스키마 설정 ,env 등

--- next.config.js
+++ next.config.js
@@ -4,6 +4,8 @@ const nextConfig = {
   reactStrictMode: true,
   env: {
     KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+    GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
+    GOOGLE_CLIENT_SECRET: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
   },
 };
 
