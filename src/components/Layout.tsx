6d4d550d4b93432b52a0857332e5987e2d2fe85b
yoon3n4m <lgmysn2@gmail.com>
2023-08-21 14:01:19 +0900

    feat: 최초 로그인 후 닉네임 여부 확인 로직 구현 완료

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,6 +1,6 @@
 import Navigator from "./Navigator";
 import { StateContext } from "@/util/StateContext";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useRouter } from "next/router";
 
 import GlobalStyles from "@/styles/GlobalStyles";
@@ -21,9 +21,22 @@ export default function Layout({ children }: React.PropsWithChildren) {
   const isMapPage = router.asPath === "/map";
   const session: any = useSession();
 
-  console.log(session.data?.user.nickname);
+  //로그인 이후 닉네임 여부 확인 로직
+  useEffect(() => {
+    if (session.data === undefined) return;
+    //로그인 상태가 아니면 탈출
+    if (session.status !== "authenticated") return;
+    //로그인 설정 창이면 탈출
+    if (router.pathname === "/nickname") return;
 
-  useEffec;
+    if (session.data.user.nickname === null) {
+      alert(
+        "원활한 서비스 이용을 위해 가입 이후 최초 닉네임 설정이 필요합니다. 닉네임 설정 화면으로 이동합니다."
+      );
+      router.push("/nickname");
+    }
+  }, [session.data]);
+
   return (
     <>
       <GlobalStyles />
