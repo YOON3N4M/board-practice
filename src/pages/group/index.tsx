dd9c6974993121bbb684d5492a84c4f289bc9f8d
yoon3n4m <lgmysn2@gmail.com>
2023-08-24 16:46:34 +0900

    feat: findMany inclued로 사용자가 속한 모든 그룹을 불러오는 로직,API

--- src/pages/group/index.tsx
+++ src/pages/group/index.tsx
@@ -14,11 +14,32 @@ import {
   VStack,
   Divider,
 } from "@chakra-ui/react";
+import axios from "axios";
+import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
 import { FiPlus } from "react-icons/fi";
+import { API_URL_CREATE_MEMBERSHIP } from "../_app";
+import { useState, useEffect } from "react";
+import { GroupT } from "@/@types/types";
 
 export default function Group() {
   const navigate = useRouter();
+  const session: any = useSession();
+
+  const [ownGroup, setOwnGroup] = useState<GroupT[]>([]);
+
+  async function getOwnGroupByDB() {
+    console.log("??");
+    const userId = session.data?.user?.id;
+
+    const res = await axios
+      .get(API_URL_CREATE_MEMBERSHIP, userId)
+      .then(res => setOwnGroup(res.data.groupArr));
+  }
+
+  useEffect(() => {
+    getOwnGroupByDB();
+  }, []);
 
   return (
     <>
@@ -81,134 +102,41 @@ export default function Group() {
               </VStack>
             </Center>
           </Card>
-          <Card
-            borderRadius={"4px"}
-            shadow={""}
-            bgColor="whiteAlpha.800"
-            minW="170px"
-            minH="180px"
-            justifyContent={"center"}
-            boxShadow={"md"}
-            cursor={"pointer"}
-            onClick={() => {
-              navigate.push("/creategroup");
-            }}
-          >
-            <Center w={"100%"} h={"100%"}>
-              <VStack spacing={"13px"}>
-                <Center>
-                  <Box
-                    w="60px"
-                    h="60px"
-                    bgColor={"black"}
-                    lineHeight="60px"
-                    borderRadius={"50%"}
-                  >
-                    +
-                  </Box>
+
+          {ownGroup.length !== 0 &&
+            ownGroup.map(group => (
+              <Card
+                key={group.id}
+                borderRadius={"4px"}
+                shadow={""}
+                bgColor="whiteAlpha.800"
+                minW="170px"
+                minH="180px"
+                justifyContent={"center"}
+                boxShadow={"md"}
+                cursor={"pointer"}
+                onClick={() => {
+                  navigate.push(`/map/${group.id}`);
+                }}
+              >
+                <Center w={"100%"} h={"100%"}>
+                  <VStack spacing={"13px"}>
+                    <Center>
+                      <Center
+                        w="60px"
+                        h="60px"
+                        bgColor={group.group_cover}
+                        lineHeight="60px"
+                        borderRadius={"50%"}
+                      ></Center>
+                    </Center>
+                    <Box>
+                      <Text>{group.name}</Text>
+                    </Box>
+                  </VStack>
                 </Center>
-                <Box>
-                  <Text>새 그룹 만들기</Text>
-                </Box>
-              </VStack>
-            </Center>
-          </Card>
-          <Card
-            borderRadius={"4px"}
-            shadow={""}
-            bgColor="whiteAlpha.800"
-            minW="170px"
-            minH="180px"
-            justifyContent={"center"}
-            boxShadow={"md"}
-            cursor={"pointer"}
-            onClick={() => {
-              navigate.push("/creategroup");
-            }}
-          >
-            <Center w={"100%"} h={"100%"}>
-              <VStack spacing={"13px"}>
-                <Center>
-                  <Box
-                    w="60px"
-                    h="60px"
-                    bgColor={"black"}
-                    lineHeight="60px"
-                    borderRadius={"50%"}
-                  >
-                    +
-                  </Box>
-                </Center>
-                <Box>
-                  <Text>새 그룹 만들기</Text>
-                </Box>
-              </VStack>
-            </Center>
-          </Card>
-          <Card
-            borderRadius={"4px"}
-            shadow={""}
-            bgColor="whiteAlpha.800"
-            minW="170px"
-            minH="180px"
-            justifyContent={"center"}
-            boxShadow={"md"}
-            cursor={"pointer"}
-            onClick={() => {
-              navigate.push("/creategroup");
-            }}
-          >
-            <Center w={"100%"} h={"100%"}>
-              <VStack spacing={"13px"}>
-                <Center>
-                  <Box
-                    w="60px"
-                    h="60px"
-                    bgColor={"black"}
-                    lineHeight="60px"
-                    borderRadius={"50%"}
-                  >
-                    +
-                  </Box>
-                </Center>
-                <Box>
-                  <Text>새 그룹 만들기</Text>
-                </Box>
-              </VStack>
-            </Center>
-          </Card>
-          <Card
-            borderRadius={"4px"}
-            shadow={""}
-            bgColor="whiteAlpha.800"
-            minW="170px"
-            minH="180px"
-            justifyContent={"center"}
-            boxShadow={"md"}
-            cursor={"pointer"}
-            onClick={() => {
-              navigate.push("/creategroup");
-            }}
-          >
-            <Center w={"100%"} h={"100%"}>
-              <VStack spacing={"13px"}>
-                <Center>
-                  <Box
-                    w="60px"
-                    h="60px"
-                    bgColor={"black"}
-                    lineHeight="60px"
-                    borderRadius={"50%"}
-                  >
-                    +
-                  </Box>
-                </Center>
-                <Box>
-                  <Text>새 그룹 만들기</Text>
-                </Box>
-              </VStack>
-            </Center>
-          </Card>
+              </Card>
+            ))}
         </HStack>
         <Text fontWeight={"bold"} mb={"10px"}>
           내 타임라인
