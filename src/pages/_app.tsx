3e99b9f1bab56b4ca3c17a620f28bb74555a7dc0
yoon3n4m <lgmysn2@gmail.com>
2023-08-25 15:36:34 +0900

    feat: 초대링크 생성 API, 앞단 작업

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,6 +7,7 @@ export const API_URL_USER_DATA = "/api/user-data";
 export const API_URL_CHANGE_NICKNAME = "/api/nickname";
 export const API_URL_CREATE_GROUP = "/api/group";
 export const API_URL_CREATE_MEMBERSHIP = "/api/membership";
+export const API_URL_INVITE = "/api/invite";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
---
+++ src/pages/api/invite.ts
@@ -0,0 +1,34 @@
+import prisma from "@/util/prismaClient";
+import type { NextApiRequest, NextApiResponse } from "next";
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const { inviteCode, groupId, expired } = req.body;
+    try {
+      const existCode = await prisma.invite.findUnique({
+        where: {
+          inviteCode: inviteCode,
+        },
+      });
+
+      if (existCode) {
+        res.status(400).json({ message: "중복 코드 존재" });
+        return;
+      }
+
+      const inviteCodeRes = await prisma.invite.create({
+        data: {
+          groupId: groupId,
+          inviteCode: inviteCode,
+          expired: expired,
+        },
+      });
+      res.status(200).json({ message: "200, 등록 성공", inviteCodeRes });
+    } catch (err) {
+      res.status(500).json({ message: "500, 등록 실패" });
+    }
+  }
+}
