69fda39fc5ad00d111a810fd98651bc9a7df571b
yoon3nam <lgmysn2@gmail.com>
2023-08-26 17:50:03 +0900

    fix: 배포 에러 해결

--- src/pages/api/favorite.ts
+++ src/pages/api/favorite.ts
@@ -2,61 +2,73 @@ import prisma from "@/util/prismaClient";
 import type { NextApiRequest, NextApiResponse } from "next";
 
 export default async function handler(
-    req: NextApiRequest,
-    res: NextApiResponse
-  ) {
-      const { id, name, themeId, groupid, address, participants, author, date, memo, photo, placeName} = req.body;
-      
-      if (req.method === "POST") {
-          try {
-        const user = await prisma.favorite.create({
-          data: {
-            id: id, 
-            name: name, 
-            groupid: groupid,
-            themeId: themeId, 
-            address: address, 
-            participants: participants, 
-            author: author, 
-            date: date, 
-            memo: memo, 
-            photo: photo, 
-            placeName: placeName
-          },
-        });
-        res.status(200).json({ message: "200, 등록 성공" });
-      } catch (err) {
-        res.status(500).json({ message: "500, 등록 실패" });
-      }
-    } else if (req.method === "GET") {
-      try {
-        const allUsers = await prisma.favorite.findMany();
-        res.status(200).json(allUsers);
-      } catch (err) {
-        res.status(500).json({ message: "500, 가져오기 실패" });
-      }
-    } else if (req.method === "DELETE") {
-      try {
-        const deleteTarget = await prisma.favorite.delete({
-          where: {
-            //user_id: req.body,
-          },
-        });
-        res.status(200).json({ message: "200, 삭제 성공" });
-      } catch (err) {
-        res.status(500).json({ message: "500, 등록 실패" });
-      }
-    } else if (req.method === "PUT") {
-      const updateUser = await prisma.favorite.update({
-        where: {
-          //user_id: id,
-        },
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  const {
+    id,
+    name,
+    themeId,
+    groupid,
+    address,
+    participants,
+    author,
+    date,
+    memo,
+    photo,
+    placeName,
+  } = req.body;
+
+  if (req.method === "POST") {
+    try {
+      const user = await prisma.favorite.create({
         data: {
-          //해당부분도 수정 해야함
-          // user_name: "Franz",
+          id: id,
+          name: name,
+          // groupid: groupid,
+          themeId: themeId,
+          address: address,
+          participants: participants,
+          author: author,
+          date: date,
+          memo: memo,
+          photo: photo,
+          placeName: placeName,
         },
       });
-    } else {
-      res.status(405).json({ message: "405, 메소드가 없어" });
+      res.status(200).json({ message: "200, 등록 성공" });
+    } catch (err) {
+      res.status(500).json({ message: "500, 등록 실패" });
     }
+  } else if (req.method === "GET") {
+    try {
+      const allUsers = await prisma.favorite.findMany();
+      res.status(200).json(allUsers);
+    } catch (err) {
+      res.status(500).json({ message: "500, 가져오기 실패" });
+    }
+  } else if (req.method === "DELETE") {
+    try {
+      const deleteTarget = await prisma.favorite.delete({
+        where: {
+          //user_id: req.body,
+        },
+      });
+      res.status(200).json({ message: "200, 삭제 성공" });
+    } catch (err) {
+      res.status(500).json({ message: "500, 등록 실패" });
+    }
+  } else if (req.method === "PUT") {
+    const updateUser = await prisma.favorite.update({
+      where: {
+        //user_id: id,
+      },
+      data: {
+        //해당부분도 수정 해야함
+        // user_name: "Franz",
+      },
+    });
+  } else {
+    res.status(405).json({ message: "405, 메소드가 없어" });
   }
+}
