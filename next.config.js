c185b77634b701154298174281416a186baa136f
윤세남 <lgmysn2@gmail.com>
2024-01-05 13:28:02 +0900

    feat: 맵 네비게이터 구조 잡기 및 env 설정

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
   reactStrictMode: false,
   env: {
     KAKAO_API_KEY: process.env.NEXT_PUBLIC_KAKAO_API_KEY,
+    NAVER_API_KEY: process.env.NEXT_PUBLIC_NAVER_API_KEY,
     GOOGLE_CLIENT_ID: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET,
     NEXTAUTH_SECRET: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -7,7 +7,7 @@ export default function Document() {
       <Head>
         <Script
           type="text/javascript"
-          src="https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=f8ijn3o0md"
+          src={`https://oapi.map.naver.com/openapi/v3/maps.js?ncpClientId=${process.env.NAVER_API_KEY}`}
           strategy="beforeInteractive"
         ></Script>
       </Head>
